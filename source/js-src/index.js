6782011dfca79df9e13864e196fbe1f8fe34b68e
ibzieg <ian.b.zieg@gmail.com>
2018-08-31 00:38:06 -0700

    added HoeflerText

--- source/js-src/index.js
+++ source/js-src/index.js
@@ -1,8 +1,11 @@
 const AnimationManager = require("./animation-manager");
-const ProjectDetailRenderer = require("./project-detail-renderer");
-
+const ProjectDetailRenderer = require("./project-detail-view");
+const PortfolioView = require("./portfolio-view");
 
 window.onload = () => {
+    let portfolio = new PortfolioView();
+    portfolio.render();
+
     let projectDetail = new ProjectDetailRenderer();
     projectDetail.render();
 
---
+++ source/js-src/portfolio-view.js
@@ -0,0 +1,10 @@
+const $ = require("jquery");
+
+class PortfolioView {
+
+    render() {
+        $(".project-title-wrapper a").addClass("palette-black-fg");
+    }
+
+}
+module.exports = PortfolioView;
